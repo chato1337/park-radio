d793003fd3ca1feeb57fe9f01f83415bf440646f
chato1337 <didier.cuetia@appspring.tech>
2021-05-05 22:18:41 -0500

    ui support

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.scss";
 import Logo from "../../logo.svg";
 
 const Navbar = () => {
+	const [darkMode, setDarkMode] = useState(false);
+
+	const handleClick = () => {
+		setDarkMode(!darkMode);
+	};
+
 	return (
 		<div className="navbar">
 			<div className="navbar__logo">
 				<img src={Logo} alt="logo image" />
 			</div>
+			<button type="button" onClick={handleClick}>
+				{darkMode ? "Dark Mode" : "Light Mode"}
+			</button>
 		</div>
 	);
 };
